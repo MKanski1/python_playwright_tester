5dea106dacdbaab9742ebffd36ead80f2b8ff669
MKanski1 <54279998+MKanski1@users.noreply.github.com>
2024-10-21 15:56:32 +0200

    Update test_play1.py

--- tests/test_play1.py
+++ tests/test_play1.py
@@ -1,35 +1,32 @@
-from playwright.sync_api import sync_playwright, expect
-import pytest
+import asyncio
+
+from playwright.async_api import async_playwright
 
 
-@pytest.fixture(scope='function')
-def browser_context():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
-        context = browser.new_context()
-        yield context
-        context.close()
-        browser.close()
+async def run():
+  async with async_playwright() as p:
+    # Launch the browser
+    browser = await p.chromium.launch()
+    page = await browser.new_page()
+
+    # Navigate to the playwright homepage
+    await page.goto('https://playwright.dev/')
+
+    # Print the home page website's title
+    print('Playwright home page title: "%s"' % await page.title())
+
+    # Navigate to the documentation page
+    await page.get_by_role('link', name='Get started').click()
+
+    # Print the documentation page website's title
+    print('Documentation page title: "%s"' % await page.title())
+
+    # Take a screenshot
+    await page.screenshot(path='screenshot.png')
+    print('Screenshot captured and saved as screenshot.png')
+
+    # Close the browser
+    await browser.close()
 
 
-def test_run(browser_context):
-    page = browser_context.new_page()
-    page.goto('https://www.alx.pl')
-    page.screenshot(path="screenshot/obraz1.png")
-    assert page.title() == "ALX – szkoła programowania, szkolenia IT i kursy informatyczne"
-
-
-def test_example(browser_context):
-    page = browser_context.new_page()
-    page.goto("https://www.alx.pl/")
-    page.get_by_role("textbox").click()
-    page.get_by_role("textbox").fill("devops")
-    page.get_by_role("textbox").press("Enter")
-    page.get_by_role("link", name="Devops - AWS, Terraform,").click()
-    page.get_by_text("Osobom zainteresowanym").click()
-    page.get_by_role("link", name="Zapisz się").click()
-    page.wait_for_timeout(1000)
-    page.screenshot(path="screenshot/obraz2.png")
-    expect(page.get_by_label("Termin*")).to_be_visible()
-    expect(page.locator("#article")).to_contain_text("Devops - AWS, Terraform, Monitoring DEVOPS-102")
-
+asyncio.run(run())
